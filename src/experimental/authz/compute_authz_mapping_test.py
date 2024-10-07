efe6fc1a48d859e20d992c286525f0db8317466d
Dom Del Nano <ddelnano@gmail.com>
2024-10-07 21:45:03 +0000

    Add filtering for static list of substrings to reduce non service traffic (healthchecks, etc)
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>

--- src/experimental/authz/compute_authz_mapping_test.py
+++ src/experimental/authz/compute_authz_mapping_test.py
@@ -16,15 +16,17 @@ def test_authz_service_mapping(spark_fixture):
     data_source = spark_fixture.read.json("data/otel-export-authz-source.json")
     df = compute_authz_service_mapping(data_source)
 
+    # The test file contains a ton of data, but we only care to match the rough shape of the output
     row = df.count().head()
     single_row_df = spark_fixture.createDataFrame([row])
 
     expected = [{
-        "client_name": "",
-        "service_name": "kube-dns",
-        "http_target": "/readiness",
-        "http_method": "GET",
-        "count": 29,
+        "client_name": "vizier-query-broker",
+        "service_name": "vizier-metadata-svc",
+        "http_target": "/px.vizier.services.metadata.CronScriptStoreService/RecordExecutionResult",
+        "http_method": "POST",
+        "count": 15,
     }]
+    # The .select forces the columns to be in the same order as df
     expected_df = spark_fixture.createDataFrame(expected).select("service_name", "client_name", "http_target", "http_method", "count")
     assertDataFrameEqual(single_row_df, expected_df)
